9e281d621f00c212bdcd96e882338e8be420f7e3
Ferryloy <141764436+Ferryloy@users.noreply.github.com>
2024-12-13 14:04:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2347026305811"
-global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
+global.ownernomer = "2347047787798"
+global.ownername = "Ferryloy"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America, Haiti, Port-au-Prince"
 
-global.ownernumber = '2347026305811'  //creator number
-global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
+global.ownernumber = '2347047787798'  //creator number
+global.ownername = 'Ferryloy' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
 //sticker details
